d6ed43fd744cfda49ddf4e7086e9de80c7ec6f28
P.S.R.Fernando <110447549+IT21823398@users.noreply.github.com>
2022-08-09 15:58:35 +0000

    changed input and output commands with precision

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,47 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
 int main()
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
-
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+  double salary, netSalary;         //declare variables
+  int etype, otHrs, otRate;
   
-   return 0;
+  std::cout << "Enter Employee Type : ";
+  std::cin >> etype;
+  
+  std::cout << "Enter Salary : ";
+  std::cin >> salary;
+  
+  std::cout << "Enter OtHrs : ";
+  std::cin >> otHrs;
+   
+  switch (etype) {                //determine the overtime rate paid
+    case 1 :
+      otRate = 1000;
+      break;
+    
+    case 2 :
+      otRate = 1500;
+      break;
+    
+    default :
+      otRate = 1700;
+      
+  }
+
+  std::cout << "\n";
+
+  netSalary = salary + (otHrs* otRate);       //calculate net salary
+
+  std::cout << "Net Salary : ";  //display net salary
+  
+  std::cout << std::setw(10) << std::setiosflags(ios::fixed) << std::setprecision(2) << netSalary;      
+  
+  return 0;
+  
 }
