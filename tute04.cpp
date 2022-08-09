34497a69834bd9fb001237a49dc97808ea1c9a1c
P.S.R.Fernando <110447549+IT21823398@users.noreply.github.com>
2022-08-09 17:00:36 +0000

    returned the factorial upon decleration to the nCr function

--- tute04.cpp
+++ tute04.cpp
@@ -11,19 +11,48 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+#include<iostream>
 
-long Factorial(int no);
-long nCr(int n, int r);
+long Factorial(int n){              //calculate factorial
+  int i , fac = 1;
+
+  for(i = n ; i >= 1 ; i--){
+    fac = fac * i;
+
+  }
+
+  return fac;                       //return factorial 
+    
+}
+  
+long nCr(int n, int r){            //calculate nCr
+  int facN , facR , facNR;
+
+  facN = Factorial(n);             //calculate n!
+  facR = Factorial(r);             //calculate r!
+  facNR = Factorial(n - r);        //calculate (n-r)!
+
+  return facN / (facR * facNR);        //return result
+  
+}
 
 int main() {
   int n, r;
+  
   std::cout << "Enter a value for n ";
   std::cin >> n;
+  
   std::cout << "Enter a value for r ";
   std::cin >> r;
+  
   std::cout << "nCr = ";
+  
   std::cout << nCr(n,r);
+  
   std::cout << std::endl;
+  
   return 0;
+  
 }
+
+
